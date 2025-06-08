0164a5021fccef4329c2c4cec529a3ff4afe7af1
DiegoB1911 <diego.barquero@hotmail.com>
2025-06-08 13:20:51 -0600

    feat: adjusting implementation

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Noyra',
+  description: 'Noyra',
+  generator: 'Noyra',
 }
 
 export default function RootLayout({
